605080b577523eb8171c97e1942a7dc5b4599837
fpalomerosanz <134016706+fpalomeeosanz@users.noreply.github.com>
2023-08-31 16:06:27 -0400

    depuracion de codigo comparte.js

--- comparte.js
+++ comparte.js
@@ -10,36 +10,56 @@ const baseDeIdeas = [{
 {
     usuario: 'joaquin' ,
     nuevaIdeaCuento: 'un panda que se desdobla para resolver eacuaciones imposibles en librerias abandonadas', 
-},
+},    
 ];
 
 //objeto para contener info nuevas ideas
 const ideaDeUser = {
-    usuario: '' ,
-    nuevaIdeaCuento: '' ,
+    usuario: '',
+    nuevaIdeaCuento: '',
 };
 
-//conecion con html
+//conecion con html o DOM
 const buttonIdeas = document.getElementById("buttonIdeas");
-const cuadroMensaje = document.getElementById ("cuadroMensaje");
-const inputs = document.querySelectorAll("imput")
+const inputUsuario = document.getElementById("inputUsuario");
+const inputIdea = document.getElementById("inputIdea");
 
-//boton para enviar iddeas
-inputs.forEach((ele)=> {
-    ele.addEventListener("input",()=>{
-        buttonIdeas.addEventListener("click",()=>{
-            ideaDeUser.push(baseDeIdeas);
-        });
-    });
+//darle funcionn al evento clic y Uso de TRIM para limpiar datos depues 
+buttonIdeas.addEventListener("click", () => {
+    const usuario = inputUsuario.value.trim();
+    const nuevaIdea = inputIdea.value.trim();
+
+    if (usuario === '' || nuevaIdea === '') {
+        swal("Para que podamos gauardar tu idea debes poner un nombre y una idea ;)");;
+        
+        return;
+    }
+
+    ideaDeUser.usuario = usuario;
+    ideaDeUser.nuevaIdeaCuento = nuevaIdea;
+
+    //para guardar las nuevas ideas en el array
+    baseDeIdeas.push({...ideaDeUser});
+
+    //para que queden en el localstorage
+    localStorage.setItem("baseDeIdeas", JSON.stringify(baseDeIdeas));
+
+    //para limpiar los valores al user nuevo
+    inputUsuario.value = '';
+    inputIdea.value = '';
+
+    //texto de con biblioteca de sweet alert
+    swal("WOW!!! Que buena idea!!! Ya la guardamos, recuerda estar ateto para saber si la tuya será la proxima!");
 });
 
-//guardado en local storage
-localStorage.setItem("usuario","nuevaIdeaCuento");
+//para guardar las ideas del localstorage o como respaldo
+const ideasEnBruto = JSON.parse(localStorage.getItem("baseDeIdeas")) || [];
 
-//JSON
-const ideasEnBruto = localStorage.getItem("baseDeIdeas",JSON.stringify(baseDeIdeas));
-JSON.parse(localStorage.getItem("ideasEnBruto"))
+if (ideasEnBruto.length > 0) {
+    ideasEnBruto.forEach(idea => {
+        baseDeIdeas.push(idea);
+    });
+}
+
 console.log(baseDeIdeas)
 console.log(ideasEnBruto)
-
-
