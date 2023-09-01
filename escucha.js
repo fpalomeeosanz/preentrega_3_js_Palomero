1894470d0337a013c45eb8cddb935e0b91c881fa
fpalomerosanz <134016706+fpalomeeosanz@users.noreply.github.com>
2023-08-31 20:11:15 -0400

    alerta not found escucha.js

--- escucha.js
+++ escucha.js
@@ -1,15 +1,36 @@
 //busqueda y preparacion de audios 
 let queryCuentos = window.location.search.substring(1).split("=");
 let audioEscucha = document.getElementById("audioEscucha");
+
 //empate de busquedas y play
 audioEscucha.src = "../audios/" + queryCuentos[1];
 audioEscucha.play();
-//para el problema del autoplay bloqueado 
-function handlePlayButton() {
-    if (audioEscucha.paused) {
-      play();
-    } else {
+
+//para el problema del autoplay bloqueado ayuda de documentacion
+audioEscucha.addEventListener("play", function() {
+  function handlePlayButton() {
+  if (audioEscucha.paused) {
+      audioEscucha.play();
+  } else {
       audioEscucha.pause();
-      playButton.audioEscucha = "";
-    }
   }
+}
+});
+
+// Muestra una alerta si ocurre un error de carga
+audioEscucha.addEventListener('error', function () {
+  swal({
+    title: "UPS!! Estamos trabajando para ti!!!",
+    text: "Este audiocuento está en proceso, pero puedes escuchar otros en JUEGA.",
+    icon: "warning",
+    buttons: true,
+    dangerMode: true,
+})
+.then((play) => {
+    if (play) {
+        swal("¡Dale a Jugar Otra Vez!",);
+    } else {
+        swal("¡Vuelve pronto!", "Estamos agregando nuevos cuentos constantemente.", "info");
+    }
+});
+});
